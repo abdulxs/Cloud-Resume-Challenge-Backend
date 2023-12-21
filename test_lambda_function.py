9b6699b94e5ae88819cd11286207540c252b8ca0
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-21 01:06:50 +0100

    Update test_lambda_function.py
    still debugging

--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,53 +1,44 @@
 import json
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from lambda_function import lambda_handler
 
 class TestLambdaFunction(unittest.TestCase):
-    @patch('lambda_function.boto3.resource')
+    @patch('boto3.resource')
     def test_get_visitor_count_success(self, mock_dynamodb_resource):
-        # Mocking DynamoDB table and its get_item method
-        mock_table = MagicMock()
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
         mock_table.get_item.return_value = {'Item': {'visitorCount': '1', 'count': 10}}
-        mock_dynamodb_resource.return_value.Table.return_value = mock_table
 
-        # Testing Lambda function for successful GET request
         event = {'httpMethod': 'GET'}
         response = lambda_handler(event, None)
 
-        # Assertions
+        mock_dynamodb_resource.assert_called_once_with('dynamodb', region_name='eu-north-1')
         mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), {'visitorCount': 10})
 
-    @patch('lambda_function.boto3.resource')
+    @patch('boto3.resource')
     def test_get_visitor_count_error(self, mock_dynamodb_resource):
-        # Mocking DynamoDB table and causing an error in get_item method
-        mock_table = MagicMock()
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
         mock_table.get_item.side_effect = Exception('Some error')
-        mock_dynamodb_resource.return_value.Table.return_value = mock_table
 
-        # Testing Lambda function for error in GET request
         event = {'httpMethod': 'GET'}
         response = lambda_handler(event, None)
 
-        # Assertions
+        mock_dynamodb_resource.assert_called_once_with('dynamodb', region_name='eu-north-1')
         mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
         self.assertEqual(response['statusCode'], 500)
         self.assertIn('error', json.loads(response['body']))
 
-    @patch('lambda_function.boto3.resource')
+    @patch('boto3.resource')
     def test_update_visitor_count_success(self, mock_dynamodb_resource):
-        # Mocking DynamoDB table and its update_item method
-        mock_table = MagicMock()
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
         mock_table.update_item.return_value = {'Attributes': {'count': 11}}
-        mock_dynamodb_resource.return_value.Table.return_value = mock_table
 
-        # Testing Lambda function for successful POST request
         event = {'httpMethod': 'POST'}
         response = lambda_handler(event, None)
 
-        # Assertions
+        mock_dynamodb_resource.assert_called_once_with('dynamodb', region_name='eu-north-1')
         mock_table.update_item.assert_called_once_with(
             Key={'visitorCount': '1'},
             UpdateExpression='SET #count = #count + :incr',
@@ -58,18 +49,15 @@ class TestLambdaFunction(unittest.TestCase):
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), {'updatedVisitorCount': 11})
 
-    @patch('lambda_function.boto3.resource')
+    @patch('boto3.resource')
     def test_update_visitor_count_error(self, mock_dynamodb_resource):
-        # Mocking DynamoDB table and causing an error in update_item method
-        mock_table = MagicMock()
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
         mock_table.update_item.side_effect = Exception('Some error')
-        mock_dynamodb_resource.return_value.Table.return_value = mock_table
 
-        # Testing Lambda function for error in POST request
         event = {'httpMethod': 'POST'}
         response = lambda_handler(event, None)
 
-        # Assertions
+        mock_dynamodb_resource.assert_called_once_with('dynamodb', region_name='eu-north-1')
         mock_table.update_item.assert_called_once_with(
             Key={'visitorCount': '1'},
             UpdateExpression='SET #count = #count + :incr',
@@ -80,13 +68,11 @@ class TestLambdaFunction(unittest.TestCase):
         self.assertEqual(response['statusCode'], 500)
         self.assertIn('error', json.loads(response['body']))
 
-    @patch('lambda_function.boto3.resource')
+    @patch('boto3.resource')
     def test_unsupported_method(self, mock_dynamodb_resource):
-        # Testing Lambda function for an unsupported HTTP method
         event = {'httpMethod': 'PUT'}
         response = lambda_handler(event, None)
 
-        # Assertions
         self.assertEqual(response['statusCode'], 400)
         self.assertIn('error', json.loads(response['body']))
 
