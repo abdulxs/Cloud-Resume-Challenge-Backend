a0e59a44a7efd416a25e36a5aadf6813c099f686
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-21 02:41:20 +0100

    Update lambda_function.py
    added credentials

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import json
 import boto3
 
 AWS_REGION = 'eu-north-1'
+AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 
 dynamodb = boto3.resource('dynamodb',region_name=AWS_REGION)
 table = dynamodb.Table('Resume')  # Replace with your actual table name
