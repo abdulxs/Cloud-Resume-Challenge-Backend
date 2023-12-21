3d50e01807dd4ef9685ca0e233a8768bab13aa70
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-21 03:07:08 +0100

    Update lambda_function.py
    explicitly passed credentials, for testing purposes

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,10 @@ import json
 import boto3
 
 AWS_REGION = 'eu-north-1'
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+session = boto3.Session(
+    aws_access_key_id='AKIA5JN6KY22ESIUXUHU',
+    aws_secret_access_key='+EuhkCFa2JaZ1HFHVul+TgxMgd3Eqfl5fC5qdCKi'
+)
 
 dynamodb = boto3.resource('dynamodb',region_name=AWS_REGION)
 table = dynamodb.Table('Resume')  # Replace with your actual table name
