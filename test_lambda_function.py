bcf363aae704d82fa19d381014e9d4930ec9edc8
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-21 15:28:25 +0100

    Update test_lambda_function.py
    added more parameters to the event

--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,51 +1,126 @@
 import json
-import boto3
 import unittest
+from unittest.mock import patch
 from lambda_function import lambda_handler
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
-table = dynamodb.Table('Resume')  # Replace with your actual table name
+class TestLambdaFunction(unittest.TestCase):
+    @patch('boto3.resource')
+    def test_get_visitor_count_success(self, mock_dynamodb_resource):
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
+        mock_table.get_item.return_value = {'Item': {'visitorCount': '1', 'count': 10}}
 
-class TestLambdaHandler(unittest.TestCase):
-    def test_get_method(self):
+        # Construct the event for the GET method
         event = {
-            "httpMethod": "GET"
+            'httpMethod': 'GET',
+            'headers': {
+                'Host': 'https://6kk5qw05q5.execute-api.eu-north-1.amazonaws.com/development/resumeFunction'
+            },
+            'queryStringParameters': None,
+            'body': None,
+            'isBase64Encoded': False
         }
+
         response = lambda_handler(event, None)
-        print(response)
+
+        mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
         self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(response['body'], json.dumps({'visitorCount': 1}))
+        self.assertEqual(json.loads(response['body']), {'visitorCount': 10})
 
-    def test_post_method(self):
+    @patch('boto3.resource')
+    def test_get_visitor_count_error(self, mock_dynamodb_resource):
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
+        mock_table.get_item.side_effect = Exception('Some error')
+
+        # Construct the event for the GET method
         event = {
-            "httpMethod": "POST"
+            'httpMethod': 'GET',
+            'headers': {
+                'Host': 'https://6kk5qw05q5.execute-api.eu-north-1.amazonaws.com/development/resumeFunction'
+            },
+            'queryStringParameters': None,
+            'body': None,
+            'isBase64Encoded': False
         }
+
         response = lambda_handler(event, None)
-        print(response) 
+
+        mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
+        self.assertEqual(response['statusCode'], 500)
+        self.assertIn('error', json.loads(response['body']))
+
+    @patch('boto3.resource')
+    def test_update_visitor_count_success(self, mock_dynamodb_resource):
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
+        mock_table.update_item.return_value = {'Attributes': {'count': 11}}
+
+        # Construct the event for the POST method
+        event = {
+            'httpMethod': 'POST',
+            'headers': {
+                'Host': 'https://6kk5qw05q5.execute-api.eu-north-1.amazonaws.com/development/resumeFunction'
+            },
+            'queryStringParameters': None,
+            'body': None,
+            'isBase64Encoded': False
+        }
+
+        response = lambda_handler(event, None)
+
+        mock_table.update_item.assert_called_once_with(
+            Key={'visitorCount': '1'},
+            UpdateExpression='SET #count = #count + :incr',
+            ExpressionAttributeNames={'#count': 'count'},
+            ExpressionAttributeValues={':incr': 1},
+            ReturnValues='UPDATED_NEW'
+        )
         self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(response['body'], json.dumps({'updatedVisitorCount': 2}))
+        self.assertEqual(json.loads(response['body']), {'updatedVisitorCount': 11})
 
-    def test_invalid_method(self):
+    @patch('boto3.resource')
+    def test_update_visitor_count_error(self, mock_dynamodb_resource):
+        mock_table = mock_dynamodb_resource.return_value.Table.return_value
+        mock_table.update_item.side_effect = Exception('Some error')
+
+        # Construct the event for the POST method
         event = {
-            "httpMethod": "PUT"
+            'httpMethod': 'POST',
+            'headers': {
+                'Host': 'https://6kk5qw05q5.execute-api.eu-north-1.amazonaws.com/development/resumeFunction'
+            },
+            'queryStringParameters': None,
+            'body': None,
+            'isBase64Encoded': False
         }
+
         response = lambda_handler(event, None)
-        print(response) 
+
+        mock_table.update_item.assert_called_once_with(
+            Key={'visitorCount': '1'},
+            UpdateExpression='SET #count = #count + :incr',
+            ExpressionAttributeNames={'#count': 'count'},
+            ExpressionAttributeValues={':incr': 1},
+            ReturnValues='UPDATED_NEW'
+        )
+        self.assertEqual(response['statusCode'], 500)
+        self.assertIn('error', json.loads(response['body']))
+
+    @patch('boto3.resource')
+    def test_unsupported_method(self, mock_dynamodb_resource):
+        # Construct the event for an unsupported method
+        event = {
+            'httpMethod': 'PUT',
+            'headers': {
+                'Host': 'https://6kk5qw05q5.execute-api.eu-north-1.amazonaws.com/development/resumeFunction'
+            },
+            'queryStringParameters': None,
+            'body': None,
+            'isBase64Encoded': False
+        }
+
+        response = lambda_handler(event, None)
+
         self.assertEqual(response['statusCode'], 400)
-        self.assertEqual(response['body'], json.dumps({'error': 'Unsupported method', 'receivedMethod': 'PUT'}))
-
-    def test_array_of_events(self):
-        event = [
-            {'httpMethod': 'GET'},
-            {'httpMethod': 'POST'}
-        ]
-        responses = lambda_handler(event, None)
-        print(responses) 
-        self.assertEqual(len(responses), 2)
-        self.assertEqual(responses[0]['statusCode'], 200)
-        self.assertEqual(responses[0]['body'], json.dumps({'visitorCount': 1}))
-        self.assertEqual(responses[1]['statusCode'], 200)
-        self.assertEqual(responses[1]['body'], json.dumps({'updatedVisitorCount': 2}))
+        self.assertIn('error', json.loads(response['body']))
 
 if __name__ == '__main__':
     unittest.main()
