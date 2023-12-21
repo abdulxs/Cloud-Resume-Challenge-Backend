9e40ffcc0b7ff7b3c8ca03993da0bccc5caa38cf
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-21 02:10:46 +0100

    Update test_lambda_function.py
    something from bard

--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,130 +1,45 @@
 import json
+import boto3
 import unittest
-from unittest.mock import patch
-from lambda_function import lambda_handler
 
-class TestLambdaFunction(unittest.TestCase):
-    @patch('boto3.resource')
-    def test_get_visitor_count_success(self, mock_dynamodb_resource):
-        mock_table = mock_dynamodb_resource.return_value.Table.return_value
-        mock_table.get_item.return_value = {'Item': {'visitorCount': '1', 'count': 10}}
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Resume')  # Replace with your actual table name
 
-        # Test with a single event
-        single_event = {'httpMethod': 'GET'}
-        response = lambda_handler(single_event, None)
+def test_lambda_handler_get_method():
+    event = json.dumps({
+        "httpMethod": "GET"
+    })
+    response = lambda_handler(event, None)
+    assert response['statusCode'] == 200
+    assert response['body'] == json.dumps({'visitorCount': 1})
 
-        mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
-        self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(json.loads(response['body']), {'visitorCount': 10})
+def test_lambda_handler_post_method():
+    event = json.dumps({
+        "httpMethod": "POST"
+    })
+    response = lambda_handler(event, None)
+    assert response['statusCode'] == 200
+    assert response['body'] == json.dumps({'updatedVisitorCount': 2})
 
-        # Reset the mock for the next test
-        mock_table.reset_mock()
+def test_lambda_handler_invalid_method():
+    event = json.dumps({
+        "httpMethod": "PUT"
+    })
+    response = lambda_handler(event, None)
+    assert response['statusCode'] == 400
+    assert response['body'] == json.dumps({'error': 'Unsupported method', 'receivedMethod': 'PUT'})
 
-        # Test with multiple events
-        multiple_events = [{'httpMethod': 'GET'}, {'httpMethod': 'GET'}]
-        response = lambda_handler(multiple_events, None)
-
-        # Verify that the mock was called for each event in the array
-        mock_table.get_item.assert_called_with(Key={'visitorCount': '1'})
-        self.assertEqual(mock_table.get_item.call_count, 2)
-
-    @patch('boto3.resource')
-    def test_get_visitor_count_error(self, mock_dynamodb_resource):
-        mock_table = mock_dynamodb_resource.return_value.Table.return_value
-        mock_table.get_item.side_effect = Exception('Some error')
-
-        # Test with a single event
-        single_event = {'httpMethod': 'GET'}
-        response = lambda_handler(single_event, None)
-
-        mock_table.get_item.assert_called_once_with(Key={'visitorCount': '1'})
-        self.assertEqual(response['statusCode'], 500)
-        self.assertIn('error', json.loads(response['body']))
-
-        # Reset the mock for the next test
-        mock_table.reset_mock()
-
-        # Test with multiple events
-        multiple_events = [{'httpMethod': 'GET'}, {'httpMethod': 'GET'}]
-        response = lambda_handler(multiple_events, None)
-
-        # Verify that the mock was called for each event in the array
-        mock_table.get_item.assert_called_with(Key={'visitorCount': '1'})
-        self.assertEqual(mock_table.get_item.call_count, 2)
-
-    @patch('boto3.resource')
-    def test_update_visitor_count_success(self, mock_dynamodb_resource):
-        mock_table = mock_dynamodb_resource.return_value.Table.return_value
-        mock_table.update_item.return_value = {'Attributes': {'count': 11}}
-
-        # Test with a single event
-        single_event = {'httpMethod': 'POST'}
-        response = lambda_handler(single_event, None)
-
-        mock_table.update_item.assert_called_once_with(
-            Key={'visitorCount': '1'},
-            UpdateExpression='SET #count = #count + :incr',
-            ExpressionAttributeNames={'#count': 'count'},
-            ExpressionAttributeValues={':incr': 1},
-            ReturnValues='UPDATED_NEW'
-        )
-        self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(json.loads(response['body']), {'updatedVisitorCount': 11})
-
-        # Reset the mock for the next test
-        mock_table.reset_mock()
-
-        # Test with multiple events
-        multiple_events = [{'httpMethod': 'POST'}, {'httpMethod': 'POST'}]
-        response = lambda_handler(multiple_events, None)
-
-        # Verify that the mock was called for each event in the array
-        mock_table.update_item.assert_called_with(
-            Key={'visitorCount': '1'},
-            UpdateExpression='SET #count = #count + :incr',
-            ExpressionAttributeNames={'#count': 'count'},
-            ExpressionAttributeValues={':incr': 1},
-            ReturnValues='UPDATED_NEW'
-        )
-        self.assertEqual(mock_table.update_item.call_count, 2)
-
-    @patch('boto3.resource')
-    def test_update_visitor_count_error(self, mock_dynamodb_resource):
-        mock_table = mock_dynamodb_resource.return_value.Table.return_value
-        mock_table.update_item.side_effect = Exception('Some error')
-
-        # Test with a single event
-        single_event = {'httpMethod': 'POST'}
-        response = lambda_handler(single_event, None)
-
-        mock_table.update_item.assert_called_once_with(
-            Key={'visitorCount': '1'},
-            UpdateExpression='SET #count = #count + :incr',
-            ExpressionAttributeNames={'#count': 'count'},
-            ExpressionAttributeValues={':incr': 1},
-            ReturnValues='UPDATED_NEW'
-        )
-        self.assertEqual(response['statusCode'], 500)
-        self.assertIn('error', json.loads(response['body']))
-
-        # Reset the mock for the next test
-        mock_table.reset_mock()
-
-        # Test with multiple events
-        multiple_events = [{'httpMethod': 'POST'}, {'httpMethod': 'POST'}]
-        response = lambda_handler(multiple_events, None)
-
-        # Verify that the mock was called for each event in the array
-        mock_table.update_item.assert_called_with(
-            Key={'visitorCount': '1'},
-            UpdateExpression='SET #count = #count + :incr',
-            ExpressionAttributeNames={'#count': 'count'},
-            ExpressionAttributeValues={':incr': 1},
-            ReturnValues='UPDATED_NEW'
-        )
-        self.assertEqual(mock_table.update_item.call_count, 2)
-
-    # Add similar adjustments for other test methods...
+def test_lambda_handler_array_of_events():
+    event = json.dumps([
+        {'httpMethod': 'GET'},
+        {'httpMethod': 'POST'}
+    ])
+    responses = lambda_handler(event, None)
+    assert len(responses) == 2
+    assert responses[0]['statusCode'] == 200
+    assert responses[0]['body'] == json.dumps({'visitorCount': 1})
+    assert responses[1]['statusCode'] == 200
+    assert responses[1]['body'] == json.dumps({'updatedVisitorCount': 2})
 
 if __name__ == '__main__':
     unittest.main()
