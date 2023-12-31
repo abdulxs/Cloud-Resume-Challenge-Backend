5932208865b5411224bc62c9e7866223fc237872
Abdulgafar Yusuf <gaftofi@gmail.com>
2023-12-31 08:48:08 +0100

    Update lambda_function.py
    refactored and simplified

--- lambda_function.py
+++ lambda_function.py
@@ -1,75 +1,18 @@
-import os
 import json
 import boto3
-import logging
 
-AWS_REGION = 'eu-north-1'
-
-# Set up logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-dynamodb = boto3.resource('dynamodb',region_name=AWS_REGION)
+dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Resume')
 
-print(__name__)
-
 def lambda_handler(event, context):
-    print(f"Event received: {event}")
-    if isinstance(event, list):  # Check if it's an array
-        # Process each event in the array
-        responses = [process_single_event(e) for e in event]
-        return responses
-    else:
-        # Process a single event
-        return process_single_event(event)
-
-def process_single_event(event):
-    print(f"processing event: {event}")
-    # Get method (retrieve visitor count)
-    if event.get('httpMethod') == 'GET':
-        try:
-            response = table.get_item(
-                Key={'visitorCount': '1'}
-            )
-            item = response.get('Item', {})
-            count = item.get('count', 0)
-            count = float(count)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'visitorCount': count})
-            }
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': str(e)})
-            }
-
-    # Post method (update visitor count)
-    elif event.get('httpMethod') == 'POST':
-        try:
-            response = table.update_item(
-                Key={'visitorCount': '1'},
-                UpdateExpression='SET #count = #count + :incr',
-                ExpressionAttributeNames={'#count': 'count'},
-                ExpressionAttributeValues={':incr': 1},
-                ReturnValues='UPDATED_NEW'
-            )
-            updated_count = response.get('Attributes', {}).get('count', 0)
-            updated_count = float(updated_count)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'updatedVisitorCount': updated_count})
-            }
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': str(e)})
-            }
-
-    # Handle unsupported methods
-    else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Unsupported method', 'receivedMethod': event.get('httpMethod')})
-        }
+    response = table.get_item(Key={
+        'visitorCount': '1'
+    })
+    count = response['Item']['count']
+    count = count + 1
+    print(count)
+    response = table.put_item(Item={
+        'visitorCount': '1',
+        'count': count
+    })
+    return count
